6dabfb02f346b08e4226e518e4bcb39c962ac9cb
Akihiro Uchida <uchida@turbare.net>
2016-05-08 02:39:28 +0900

    reset travis tuning

--- tests/provision-ansible.sh
+++ tests/provision-ansible.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 source ~travis/virtualenv/python2.7/bin/activate
-/opt/vagrant/bin/vagrant provision ${VM} --provision-with ansible
+vagrant provision ${VM} --provision-with ansible
